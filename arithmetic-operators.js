4e674019abfa8b2599cb7a07a7a13adda2521c17
tauhidul-islam56 <tauhidulmahin@gmail.com>
2024-04-29 11:57:21 +0600

    initial commit

---
+++ arithmetic-operators.js
@@ -0,0 +1,132 @@
+// string numeric
+
+// Addition Operator
+let numberOne = "10";
+let numberTwo = "2";
+let result = numberOne + numberTwo;
+console.log(result);
+
+// Subtraction Operator
+let numberThree = "10";
+let numberFour = "2";
+let result2 = numberThree - numberFour;
+console.log(result2);
+
+// Multiplication Operator
+let numberFive = "10";
+let numberSix = "2";
+let result3 = numberFive * numberSix;
+console.log(result3);
+
+// Division Operator
+let numberSeven = "10";
+let numberEight = "2";
+let result4 = numberSeven / numberEight;
+console.log(result4);
+
+// Exponentiation Operator
+let numberNine = "10";
+let numberTen = "2";
+let result5 = numberNine ** numberTen;
+console.log(result5);
+
+// The Remainder Operator
+let numberEleven = "10";
+let numberTwelve = "2";
+let result6 = numberEleven % numberTwelve;
+console.log(result6);
+
+
+
+// String letter
+
+// Addition Operator
+let stringOne = "ten"
+let stringTwo = "two"
+let result7 = stringOne + stringTwo;
+console.log(result7);
+
+// Subtraction Operator
+let stringThree = "ten";
+let stringFour = "two";
+let result8 = stringThree - stringFour;
+console.log(result8);
+
+// Multiplication Operator
+let stringFive = "ten";
+let stringSix = "two";
+let result9 = stringFive * stringSix;
+console.log(result9);
+
+// Division Operator
+let stringSeven = "ten";
+let stringEight = "two";
+let result10 = stringSeven / stringEight;
+console.log(result10);
+
+// Exponentiation Operator
+let stringNine = "ten";
+let stringTen = "two";
+let result11 = stringNine ** stringTen;
+console.log(result11);
+
+// The Remainder Operator
+let stringEleven = "ten";
+let stringTwelve = "two";
+let result12 = stringEleven % stringTwelve;
+console.log(result12);
+
+
+
+// String letter and numeric
+
+// Addition Operator
+let string13 = "ten";
+let number14 = 5;
+let result13 = string13 + number14;
+console.log(result13);
+
+// Subtraction Operator
+let string15 = "ten";
+let number16 = 5;
+let result14 = string15 - number16;
+console.log(result14);
+
+// Multiplication Operator
+let string17 = "ten";
+let number18 = 5;
+let result15 = string17 * number18;
+console.log(result15);
+
+// Division Operator
+let string19 = "ten";
+let number20 = 5;
+let result16 = string19 / number20;
+console.log(result16);
+
+// Exponentiation Operator
+let string21 = "ten";
+let number22 = 5;
+let result17 = string21 ** number22;
+console.log(result17);
+
+// The Remainder Operator
+let string23 = "ten";
+let number24 = 5;
+let result18 = string23 % number24;
+console.log(result18);
+
+
+// String numeric and numeric
+
+// Addition Operator
+let string25 = "5";
+let number26 = 5;
+let result19 = string25 + number26;
+console.log(result19);
+
+// Subtraction Operator
+let string27 = "5";
+let number28 = 5;
+let result20 = string27 - number28;
+console.log(result20);
